70080fddb4164d76547192b0bba25590efb8a6a3
junsheng526 <junshengchoo57@gmail.com>
2023-09-03 20:47:19 +0800

    create dummy data

--- src/dao/CourseInitializer.java
+++ src/dao/CourseInitializer.java
@@ -11,9 +11,12 @@ public class CourseInitializer {
 
     public SortedListInterface<Course> initializeCourses() {
         SortedListInterface<Course> courseList = new SortedArrayList<>(); // Initialize your SortedArrayList
-        courseList.add(new Course("C1001", "FOCS", "Computer Science", 5, "Active"));
-        courseList.add(new Course("C1002", "FOET", "Engineering", 5, "Active"));
-        courseList.add(new Course("C1003", "FAFB", "Business Administration", 5, "Active"));
+        courseList.add(new Course("BACS1013", "FOCS", "PROBLEM SOLVING AND PROGRAMMING", 3, "ACTIVE"));
+        courseList.add(new Course("BACS2023", "FOCS", "OBJECT-ORIENTED PROGRAMMING", 3, "ACTIVE"));
+        courseList.add(new Course("BHEL1013", "FCCI", "ENGLISH LANGUAGE", 3, "ACTIVE"));
+        courseList.add(new Course("MPU-3322", "FSSH", "CONTEMPORARY MALAYSIAN ISSUES", 2, "ACTIVE"));
+        courseList.add(new Course("BAMS1623", "FOET", "DISCRETE MATHEMATICS", 3, "ACTIVE"));
+        courseList.add(new Course("BAIT2113", "FOCS", "WEB APPLICATION DEVELOPMENT", 3, "ACTIVE"));
         // Add more courses here
 
         return courseList;
